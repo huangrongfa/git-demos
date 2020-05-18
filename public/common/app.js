e3cb34ae80ac6f0ddc98c6189b58b2a12748a337
huangrongfa <760085259@qq.com>
2020-05-18 08:56:45 +0800

    开发学习的功能

--- public/common/app.js
+++ public/common/app.js
@@ -1,8 +1,20 @@
-window.onload = function(){
+window.onload = function () {
   this.console.log('this is hello word.....')
   demo()
+  demos()
+  test()
 }
 
-function demo () {
+function demo() {
   console.log('吃饭.....')
+}
+
+function demos() {
+  setTimeout(function () {
+    console.log('打豆豆。。。。。。')
+  }, 2000)
+}
+
+function test() {
+  console.log('学习使用我快乐')
 }
